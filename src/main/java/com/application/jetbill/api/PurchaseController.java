064c52c1115d152b6baa52d82775ecdea9e19067
jetbill <william.renteria.m@gmail.com>
2024-10-17 11:00:12 -0500

    feature(backend):inicio del flujo de compra de libros

---
+++ src/main/java/com/application/jetbill/api/PurchaseController.java
@@ -0,0 +1,10 @@
+package com.application.jetbill.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PurchaseController {
+}
